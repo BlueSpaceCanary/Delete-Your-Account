1efa177e04e7175c7673d5fb621543a0ad573cfc
Philip Davis <philip@bluespacecanary.io>
2017-11-26 19:49:56 -0800

    set up repo

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import "github.com/dghubble/oauth1"
+import "github.com/dghubble/go-twitter/twitter"
+import "io/ioutil"
+import "log"
+import "time"
+
+
+func main() {
+	ck, err := ioutil.ReadFile("consumer_key")
+	if err != nil {
+		log.Fatalf("Couldn't read consumer key: %v", err)
+	}
+
+	cs, err := ioutil.ReadFile("consumer_secret")
+	if err != nil {
+		log.Fatalf("Couldn't read consumer secret: %v", err)
+	}
+
+	at, err := ioutil.ReadFile("access_token")
+	if err != nil {
+		log.Fatalf("Couldn't read access token: %v", err)
+	}
+
+	as, err := ioutil.ReadFile("as")
+	if err != nil {
+		log.Fatalf("Couldn't read access secret: %v", err)
+	}
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	// Loading followers is mandatory on startup
+	followers := getFollowers(cl)
+	if followers == nil {
+		log.Fatal("Failed to load followers at startup")
+	}
+
+	
+}
+
+// On startup we load our follower list, and once a day we reload it
+func getFollowers(cl twitter.Client) []string {
+	params := &twitter.FollowerListParams{ScreenName: "DeleteEveryAcct"}
+	followers, _, err := cl.Followers.List(params)
+	if err != nil {
+		log.Errorf("Failed to get followers: %v")
+		return nil
+	}
+	
+	var fol []string
+	for _, user := range followers {
+		fol = append(fol, user.ScreenName)
+	}
+	cursor := followers.NextCursor
+
+	for ; ; cursor != 0 {
+		params.Cursor = cursor
+		followers, res, err := cl.Followers.List(params)
+		if res == 429 {
+			// sleep 15 minutes, then rerun
+			time.Sleep(time.Minute * 15)
+			continue
+		}
+		
+		// if we succeded, update cursor and keep going
+		cursor = followers.cursor
+		for _, user := range followers {
+			fol = append(fol, user.ScreenName)
+		} 
+	}
+
+	return fol
+}
+
+
+func loop(cl twitter.Client, followers []string) {
+	
+}
